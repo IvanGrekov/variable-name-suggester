1bd05f16bc2a3a8f7ce2a4dfe9c853df2480826f
Ivan Grekov <ivan.grekov@windmill.digital>
2023-10-18 22:34:07 +0300

    send button animation implemented

--- src/constants/animationDuration.constants.ts
+++ src/constants/animationDuration.constants.ts
@@ -1,4 +1,6 @@
 // NOTE: should be synced with `$base-animation-duration` in `src/styles/variables.scss`
 export const BASE_ANIMATION_DURATION = 1500;
+// NOTE: should be synced with `$short-animation-duration` in `src/styles/variables.scss`
+export const SHORT_ANIMATION_DURATION = 1000;
 // NOTE: should be synced with `$base-transition-duration` in `src/styles/variables.scss`
 export const BASE_TRANSITION_DURATION = 225;
--- src/features/suggester-page/components/send-message-field/SendButton.tsx
+++ src/features/suggester-page/components/send-message-field/SendButton.tsx
@@ -1,5 +1,10 @@
+import { useRef, useState, useEffect } from 'react';
+
+import cx from 'classnames';
+
 import IconButton from 'components/button/IconButton';
 import SendIcon from 'components/icons/SendIcon';
+import { SHORT_ANIMATION_DURATION } from 'constants/animationDuration.constants';
 import styles from 'features/suggester-page/components/send-message-field/SendMessageField.module.scss';
 import { useSelectAddSuggesterChatMessage } from 'features/suggester-page/stores/suggester-chat/selectors';
 import { EUserRole } from 'types/user.types';
@@ -17,7 +22,22 @@ export default function SendButton({
     setValue,
     setError,
 }: ISendButtonProps): JSX.Element {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const [isAnimation, setIsAnimation] = useState(false);
+
     const addSuggesterChatMessage = useSelectAddSuggesterChatMessage();
+
+    useEffect(() => {
+        if (isAnimation) {
+            const timeout = setTimeout(() => {
+                setIsAnimation(false);
+            }, SHORT_ANIMATION_DURATION);
+
+            return (): void => {
+                clearTimeout(timeout);
+            };
+        }
+    }, [isAnimation]);
 
     const onClick = (): void => {
         if (!value) {
@@ -25,16 +45,22 @@ export default function SendButton({
         } else {
             addSuggesterChatMessage({ text: value, userRole: EUserRole.USER });
             setValue('');
+            setIsAnimation(true);
+            buttonRef.current?.blur();
         }
     };
 
     return (
         <IconButton
+            ref={buttonRef}
             Icon={SendIcon}
             title="Send"
             isDisabled={!!error}
             onClick={onClick}
-            className={styles['send-button']}
+            className={cx(styles['send-button'], {
+                [styles['send-button--disabled']]: error,
+                [styles['send-button--animation']]: isAnimation,
+            })}
         />
     );
 }
